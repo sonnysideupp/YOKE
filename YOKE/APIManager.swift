00eae40e8422f84ae9c7eb6b650954eba626901f
sonnysideupp <shengh@princeton.edu>
2020-08-13 19:47:49 -0400

    first commit

---
+++ YOKE/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  YOKE
+//
+//  Created by Sonny Huang on 8/13/20.
+//  Copyright © 2020 Sonny Huang. All rights reserved.
+//
+
+import Foundation
---
+++ YOKE/ModalView.swift
@@ -0,0 +1,9 @@
+//
+//  ModalView.swift
+//  YOKE
+//
+//  Created by Sonny Huang on 8/13/20.
+//  Copyright © 2020 Sonny Huang. All rights reserved.
+//
+
+import Foundation
